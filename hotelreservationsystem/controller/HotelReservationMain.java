0f30385c3ce873562d7399aa4dffc1371840698c
dev01997D <devnandan01997@gmail.com>
2020-10-12 22:20:06 +0530

    Added: Hotel with price for regular customer functionality added

--- hotelreservationsystem/controller/HotelReservationMain.java
+++ hotelreservationsystem/controller/HotelReservationMain.java
@@ -5,18 +5,22 @@ import com.blz.hotelreservationsystem.model.HotelLogBook;
 
 public class HotelReservationMain {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Hotel Reservation!");
-		
-		//Adding hotel details for regular customer
-		Hotel lakeWood=new Hotel("LakeWood", 110, 90);
-		Hotel bridgeWood=new Hotel("BridgeWood", 160, 60);
-		Hotel ridgeWood=new Hotel("RidgeWood", 220, 150);
-		HotelLogBook hotelLog=new HotelLogBook();
+	// Adding hotel details for regular customer
+	private static void addHotelWithRegularCustomerPrice() {
+		Hotel lakeWood = new Hotel("LakeWood", 110, 90);
+		Hotel bridgeWood = new Hotel("BridgeWood", 160, 60);
+		Hotel ridgeWood = new Hotel("RidgeWood", 220, 150);
+		HotelLogBook hotelLog = new HotelLogBook();
 		hotelLog.addHotelToHotelLogBook(lakeWood);
 		hotelLog.addHotelToHotelLogBook(bridgeWood);
 		hotelLog.addHotelToHotelLogBook(ridgeWood);
 
-		hotelLog.getHotelBook().stream().forEach((hotel)-> System.out.println(hotel));;
+		hotelLog.getHotelBook().stream().forEach((hotel) -> System.out.println(hotel));
+		;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation!");
+		addHotelWithRegularCustomerPrice();
 	}
 }
